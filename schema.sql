ba14841254c92881a86ed2835ef9ac5b2f6066f4
gisa chris <gisachris24@gmail.com>
2023-07-19 22:08:47 -0700

    Make Required Changes

--- schema.sql
+++ schema.sql
@@ -5,9 +5,21 @@ CREATE TABLE animals(
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL(18,2),
-    species_id INT FOREIGN KEY,
-    owners_id INT FOREIGN KEY
+    species VARCHAR
 );
+
+/*Remove column species*/
+ALTER TABLE animals DROP species;
+
+/*Add column species_id which is a foreign key referencing species table*/
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD 
+CONSTRAINT SP_FKEY FOREIGN KEY(species_id) REFERENCES species(id);
+
+/*Add column owner_id which is a foreign key referencing the owners table*/
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD 
+CONSTRAINT OW_FKEY FOREIGN KEY (owner_id) REFERENCES owners(id);
 
 CREATE TABLE owners (
 id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
